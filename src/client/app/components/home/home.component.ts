f248db26001f096075e55b09a8885dc34123bce8
Luiz Silveira <zertyz@gmail.com>
2016-09-21 22:09:16 -0300

    setup

---
+++ src/client/app/components/home/home.component.ts
@@ -0,0 +1,40 @@
+// libs
+import { Store } from '@ngrx/store';
+
+// app
+import { BaseComponent, RouterExtensions } from '../../frameworks/core/index';
+import { NameListService } from '../../frameworks/sample/index';
+
+@BaseComponent({
+  moduleId: module.id,
+  selector: 'sd-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomeComponent {
+  public newName: string = '';
+  constructor(private store: Store<any>, public nameListService: NameListService, public routerext: RouterExtensions) {
+
+  }
+
+  /*
+   * @param newname  any text as input.
+   * @returns return false to prevent default form submit behavior to refresh the page.
+   */
+  addName(): boolean {
+    this.nameListService.add(this.newName);
+    this.newName = '';
+    return false;
+  }
+
+  readAbout() {
+    // Try this in the {N} app
+    // {N} can use these animation options
+    this.routerext.navigate(['/about'], {
+      transition: {
+        duration: 1000,
+        name: 'slideTop',
+      }
+    });
+  }
+}
